8a1864c963acc925f196f59eebff28a24e51000e
Stefan Paulus <32550080+SPaulusOfficial@users.noreply.github.com>
2022-03-10 00:34:36 +0100

    Update OAuthConfigChecker.java

--- src/main/java/com/andyinthecloud/githubsfdeploy/config/OAuthConfigChecker.java
+++ src/main/java/com/andyinthecloud/githubsfdeploy/config/OAuthConfigChecker.java
@@ -12,13 +12,13 @@ public class OAuthConfigChecker implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //If the environment variables are not set, send them to a "error" page that will tell them to add this to 
     	//their environment
-        if (request.getHeader(X_FORWARDED_PROTO) != null) {
+     /*   if (request.getHeader(X_FORWARDED_PROTO) != null) {
       if (request.getHeader(X_FORWARDED_PROTO).indexOf("https") != 0) {
         String pathInfo = (request.getPathInfo() != null) ? request.getPathInfo() : "";
         response.sendRedirect("https://" + request.getServerName() + pathInfo);
         return;
       }
-    }
+    }*/
 
         
     	if (System.getenv("SFDC_OAUTH_CLIENT_ID") == null || System.getenv("SFDC_OAUTH_CLIENT_SECRET") == null) {
